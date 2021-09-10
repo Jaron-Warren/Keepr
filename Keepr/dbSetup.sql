f8bfc3237802c1d5a2f92c4460c9fb92b49ac738
Jaron-Warren <Jarwarren@gmail.com>
2021-09-10 15:21:37 -0600

    db setup done

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -4,5 +4,43 @@ CREATE TABLE IF NOT EXISTS accounts(
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
   name varchar(255) COMMENT 'User Name',
   email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+  picture varchar(255) COMMENT 'User Picture',
+  Bio VARCHAR(1000) COMMENT 'Short User Bio',
+  Gender VARCHAR(255) COMMENT 'Male or Female?',
+  Country VARCHAR(255) COMMENT 'User Country'
+) default charset utf8 COMMENT '';
+-- DROP TABLE vault;
+CREATE TABLE IF NOT EXISTS vault(
+  id int NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) NOT NULL COMMENT 'Vault Name',
+  description varchar(1000) COMMENT 'Vault description',
+  isPrivate TINYINT COMMENT 'Bool value for making private',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'creator id',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS keep(
+  id int NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) NOT NULL COMMENT 'item post Name',
+  description varchar(1000) COMMENT 'item post description',
+  img VARCHAR(255) NOT NULL COMMENT 'item image',
+  views INT COMMENT 'item user views',
+  shares INT COMMENT 'item user shares',
+  keeps INT COMMENT 'item user keeps',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'creator id',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS vaultkeep(
+  id int NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'creator id',
+  vaultId INT NOT NULL COMMENT 'vault id',
+  keepId INT NOT NULL COMMENT 'keep id',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (vaultId) REFERENCES vault(id) ON DELETE CASCADE,
+  FOREIGN KEY (keepId) REFERENCES keep(id) ON DELETE CASCADE
 ) default charset utf8 COMMENT '';
